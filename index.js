8bd78e707d8d656ea1a5d78f77364fbc9bff8ab7
suibian <11126344571@qq.com>
2017-02-10 09:16:47 +0800

    fc

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+// 设置跨域访问
+app.all('*',function (req,res,next){
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+})
+app.get('/list', function (req,res){
+  res.sendFile(__dirname+'/1.json');
+});
+var server = app.listen('3002', function () {
+  console.log(server.address().address, server.address().port);
+})
