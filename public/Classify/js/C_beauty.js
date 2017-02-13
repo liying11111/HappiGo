46abc955f2d394ad40efb10d3090eac9ba691624
zxcvbnm516 <zxcvbnm516@126.com>
2017-02-13 11:28:24 +0800

    2017.2.13 classify2222

--- public/Classify/js/C_beauty.js
+++ public/Classify/js/C_beauty.js
@@ -5,4 +5,14 @@ app.controller('C_beauty',function($scope,$http){
 		$scope.arr = c.combine;
 		console.log($scope.arr)
 	})
+	
+//	$scope.face = function(){
+//		$scope.arr = c.combine;
+//		console.log($scope.arr)
+//	}
+//	$http.get('http://127.0.0.1:3002/Classify-brank').success(function(b){
+//		$scope.arr =  b.ListContents[0].GroupInfo;
+//		console.log($scope.arr)
+//	})
+	
 })
